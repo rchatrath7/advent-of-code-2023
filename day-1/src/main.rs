714c078744dd9c919473f6903b9c01beee6aad74
Rakesh Chatrath <rchatrath7@gmail.com>
2024-03-09 22:37:18 -0800

    Init and solution for day 1 first gold star

---
+++ day-1/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::read_to_string;
+
+fn main() {
+    println!("{}", read_to_string("./input.txt")
+        .unwrap()
+        .lines()
+        .map(|l| l.match_indices(|c: char| c.is_numeric()).collect::<Vec<(usize, &str)>>())
+        .map(|l| format!("{}{}", l[0].1, l[l.len() - 1].1).parse::<i64>().unwrap())
+        .sum::<i64>()
+    )
+}
